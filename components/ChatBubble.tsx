167fba53db4ef996a5279f804d0eac37f44bda87
MONU MANDAL <monumandel3@gmail.com>
2025-07-24 21:19:37 +0530

    implement ChatBubble Component

---
+++ components/ChatBubble.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Image, Text, View } from "react-native";
+
+const ChatBubble = ({ message, isMe }: any) => {
+  return (
+    // Outer container with vertical margin and horizontal alignment
+    <View className={`my-1 flex-row ${isMe ? "justify-end" : "justify-start"}`}>
+      {/* Show avatar only if the message is NOT from me */}
+      {!isMe && (
+        <Image
+          source={require("../assets/images/avatar.png")}
+          // Fake avatar image
+          className="w-8 h-8 rounded-full mr-2 self-end"
+        />
+      )}
+
+      {/* Message bubble container */}
+      <View
+        className={`
+          rounded-xl px-4 py-2 max-w-[75%]
+          ${isMe ? "bg-blue-500 self-end" : "bg-gray-100"}
+        `}
+      >
+        {/* Message text */}
+        <Text className={`${isMe ? "text-white" : "text-black"}`}>
+          {message}
+        </Text>
+      </View>
+    </View>
+  );
+};
+
+export default ChatBubble;

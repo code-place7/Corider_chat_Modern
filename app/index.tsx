95823f9f639e1967acff9d504688d30993c04cee
MONU MANDAL <monumandel3@gmail.com>
2025-07-24 21:28:19 +0530

    checking if all components works with dummy data before fetching real data

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,40 @@
-import ChatInput from "@/components/ChatInput";
-import Header from "@/components/Header";
-import { View } from "react-native";
+import { FlatList, SafeAreaView } from "react-native";
+import ChatBubble from "../components/ChatBubble";
+import ChatInput from "../components/ChatInput";
+import Header from "../components/Header";
 
-export default function Index() {
+export default function App() {
+  const messages = [
+    {
+      id: "1",
+      sender: "Rohit Yadav",
+      message: "Connect with fellow travelers...",
+      isMe: false,
+    },
+    {
+      id: "2",
+      sender: "You",
+      message: "Connect with fellow travelers...",
+      isMe: true,
+    },
+    {
+      id: "3",
+      sender: "Aliya",
+      message: "Connect with fellow travelers...",
+      isMe: false,
+    },
+  ];
+
   return (
-    <View className="flex-1 ">
+    <SafeAreaView className="flex-1 bg-white">
       <Header />
+      <FlatList
+        className="px-4"
+        data={messages}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => <ChatBubble {...item} />}
+      />
       <ChatInput />
-    </View>
+    </SafeAreaView>
   );
 }
